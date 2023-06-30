8333a55d1069cae6614de3c85a022eff58efd8b3
Ram Dettmer <ram@hungryram.com>
2023-06-30 11:44:44 -0700

    update

--- src/app/(idx)/layout.tsx
+++ src/app/(idx)/layout.tsx
@@ -1,15 +1,27 @@
+import Script from "next/script"
+
 export default function IdxLayout({
     children, // will be a page or nested layout
 }: {
     children: React.ReactNode
 }) {
     return (
-        <section>
-            <div className="section">
-                <div className="container">
-                    {children}
+        <>
+            <Script id="ihf-kestrel" src="https://kestrel.idxhome.com/ihf-kestrel.js" />
+            <Script id="ihfSCript">
+                {`window.ihfKestrel = window.ihfKestrel || {};
+	ihfKestrel.config = {
+		platform: "custom",
+		activationToken: "3779C949-155D-6043-0911FD9A208A875D",
+	};`}
+            </Script>
+            <section>
+                <div className="section">
+                    <div className="container">
+                        {children}
+                    </div>
                 </div>
-            </div>
-        </section>
+            </section>
+        </>
     )
 }
