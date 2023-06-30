e1ae7f007f9c3b01cdaa7de0de025d438fcd79b1
Ram Dettmer <ram@hungryram.com>
2023-06-30 12:27:57 -0700

    update

--- src/app/(idx)/idx/page.tsx
+++ src/app/(idx)/idx/page.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 export default function IdxPage() {
     return (
         <div className="section">
--- src/app/(idx)/listings/page.tsx
+++ src/app/(idx)/listings/page.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 export default function ListingsPage() {
     return (
         <div>
