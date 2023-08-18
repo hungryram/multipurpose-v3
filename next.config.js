a6c85328c291326d8e84cceb917ca0bffe5c1d3b
Ram Dettmer <ram@hungryram.com>
2023-08-18 11:20:21 -0700

    update

--- next.config.js
+++ next.config.js
@@ -1,5 +1,18 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
+    webpack: (config) => {
+        let modularizeImports = null;
+        config.module.rules.some((rule) =>
+          rule.oneOf?.some((oneOf) => {
+            modularizeImports =
+              oneOf?.use?.options?.nextConfig?.modularizeImports;
+            return modularizeImports;
+          }),
+        );
+        if (modularizeImports?.["@headlessui/react"])
+          delete modularizeImports["@headlessui/react"];
+        return config;
+      }, 
     // assetPrefix: 'https://multipurpose-v3.vercel.app',
     images: {
         remotePatterns: [
